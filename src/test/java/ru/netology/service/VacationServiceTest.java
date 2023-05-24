9be675aa5163ca44e0fe7b5a9fad933dc4f581c7
“Larisa” <lsergeevna1978@mail.ru>
2023-05-24 18:12:18 +0400

    Variable deleted

--- src/test/java/ru/netology/service/VacationServiceTest.java
+++ src/test/java/ru/netology/service/VacationServiceTest.java
@@ -3,7 +3,6 @@ package ru.netology.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServiceTest {
 
